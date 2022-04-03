e13569f26a611261e417dbd693800c5f48ad2925
chuma6619 <chuma6619@gmail.com>
2022-04-03 18:02:56 +0100

    deploy my react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Giftcards from './Components/Giftcards';
+
+function App() {
+  return (
+    <div className="App">
+      <Giftcards />
+    </div>
+  );
+}
+
+export default App;
